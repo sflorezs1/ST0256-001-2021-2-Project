2715da7ff2c08669ef508ff4d09c3525fb0a860c
Manuela <mherreral@eafit.edu.co>
2021-11-16 18:30:45 -0500

    Added some verifications about matrix

--- src/linear_equations.py
+++ src/linear_equations.py
@@ -3,129 +3,149 @@ import sys
 
 # Simple Gaussian elimination
 def gauss(a, b):
-    n = len(a)
-    m = (np.c_[a, b]).astype(float)
+    singular = is_singular(a)
+    if singular:
+        sys.exit('The typed matrix is not invertible')
+    else:
+        n = len(a)
+        m = (np.c_[a, b]).astype(float)
 
-    for i in range(n):
-        for j in range(i+1, n):
-            if m[i, i] != 0:
-                m[j, i:n+1] = m[j, i:n+1]-(m[j, i]/m[i, i])*m[i, i:n+1]
-            else:
-                sys.exit('Divide by zero')
-    x = back_subst(m)
+        for i in range(n):
+            for j in range(i+1, n):
+                if m[i, i] != 0:
+                    m[j, i:n+1] = m[j, i:n+1]-(m[j, i]/m[i, i])*m[i, i:n+1]
+                else:
+                    sys.exit('Divide by zero')
+        x = back_subst(m)
     return x
 
 # Gaussian elimination with complete pivoting
 def gauss_tot(a, b):
-    n = len(a)
-    m = (np.c_[a, b]).astype(float)
-    order = []
-    
-    for i in range(n):
-        aux0, b = np.where(abs(m[i:n, i:n]) == (
-            abs(m[i:n, i:n]).max()).max())
-        # column change
-        if b[0]+i != i:
-            order.append((i, b[0]+i))
-            aux2 = m[:, b[0]+i].copy()
-            m[:, b[0]+i] = m[:, i]
-            m[:, i] = aux2
-        # row change
-        if aux0[0]+i != i:
-            aux2 = m[i+aux0[0], i:n+1].copy()
-            m[aux0[0]+i, i:n+1] = m[i, i:n+1]
-            m[i, i:n+1] = aux2
-        for j in range(i+1, n):
-            if m[i, i] != 0:
-                m[j, i:n+1] = m[j, i:n+1]-(m[j, i]/m[i, i])*m[i, i:n+1]
-            else:
-                sys.exit('Divide by zero')
-    x = back_subst(m)
-    size = len(order)-1
-    for i in range(size, -1, -1):
-        aux = x[order[i][0]]
-        x[order[i][0]] = x[order[i][1]]
-        x[order[i][1]] = aux
+    singular = is_singular(a)
+    if singular:
+        sys.exit('The typed matrix is not invertible')
+    else:
+        n = len(a)
+        m = (np.c_[a, b]).astype(float)
+        order = []
+
+        for i in range(n):
+            aux0, b = np.where(abs(m[i:n, i:n]) == (
+                abs(m[i:n, i:n]).max()).max())
+            # column change
+            if b[0]+i != i:
+                order.append((i, b[0]+i))
+                aux2 = m[:, b[0]+i].copy()
+                m[:, b[0]+i] = m[:, i]
+                m[:, i] = aux2
+            # row change
+            if aux0[0]+i != i:
+                aux2 = m[i+aux0[0], i:n+1].copy()
+                m[aux0[0]+i, i:n+1] = m[i, i:n+1]
+                m[i, i:n+1] = aux2
+            for j in range(i+1, n):
+                if m[i, i] != 0:
+                    m[j, i:n+1] = m[j, i:n+1]-(m[j, i]/m[i, i])*m[i, i:n+1]
+                else:
+                    sys.exit('Divide by zero')
+        x = back_subst(m)
+        size = len(order)-1
+        for i in range(size, -1, -1):
+            aux = x[order[i][0]]
+            x[order[i][0]] = x[order[i][1]]
+            x[order[i][1]] = aux
     return x
-    
+
 
 # Gaussian elimination with parcial pivoting
 def gauss_par(a, b):
-    n = len(a)
-    m = (np.c_[a, b]).astype(float)
-    for i in range(n-1):
-        # Rows
-        aux0, aux = abs(m[i+1:n, i]).max(), (abs(m[i+1:n, i]).argmax() + 1)
-        if(aux0 > abs(m[i, i])):
-            aux1 = m[i+aux, i:n]
-            m[aux+i, i:n] = m[i, i:n]
-            m[i, i:n] = aux1
-        for j in range(i+1, n):
-            if m[j, i] != 0:
-                m[j, i:n+1] = m[j, i:n+1]-(m[j, i]/m[i, i])*m[i, i:n+1]
-            else:
-                sys.exit('Divide by zero')
-    x = back_subst(m)
+    singular = is_singular(a)
+    if singular:
+        sys.exit('The typed matrix is not invertible')
+    else:
+        n = len(a)
+        m = (np.c_[a, b]).astype(float)
+        for i in range(n-1):
+            # Rows
+            aux0, aux = abs(m[i+1:n, i]).max(), (abs(m[i+1:n, i]).argmax() + 1)
+            if(aux0 > abs(m[i, i])):
+                aux1 = m[i+aux, i:n]
+                m[aux+i, i:n] = m[i, i:n]
+                m[i, i:n] = aux1
+            for j in range(i+1, n):
+                if m[j, i] != 0:
+                    m[j, i:n+1] = m[j, i:n+1]-(m[j, i]/m[i, i])*m[i, i:n+1]
+                else:
+                    sys.exit('Divide by zero')
+        x = back_subst(m)
     return x
 
 # LU factorization with SGE
 def lu(a, b):
-    n = len(a)
-    l = np.eye(n)
-    u = np.zeros((n, n))
-    m = np.copy(a).astype(float)
+    singular = is_singular(a)
+    if singular:
+        sys.exit('The typed matrix is not invertible')
+    else:
+        n = len(a)
+        l = np.eye(n)
+        u = np.zeros((n, n))
+        m = np.copy(a).astype(float)
 
-    for i in range(n):
-        for j in range(i+1, n):
-            if m[i, i] != 0:
-                l[j, i] = m[j, i]/m[i, i]
-                m[j, i:n+1] = m[j, i:n+1]-(m[j, i]/m[i, i])*m[i, i:n+1]
-            else:
-                sys.exit('Divide by zero')
+        for i in range(n):
+            for j in range(i+1, n):
+                if m[i, i] != 0:
+                    l[j, i] = m[j, i]/m[i, i]
+                    m[j, i:n+1] = m[j, i:n+1]-(m[j, i]/m[i, i])*m[i, i:n+1]
+                else:
+                    sys.exit('Divide by zero')
 
-        u[i, i:n] = m[i, i:n]
-        u[i, i:n] = m[i, i:n]
-    u[n-1, n-1] = m[n-1, n-1]
+            u[i, i:n] = m[i, i:n]
+            u[i, i:n] = m[i, i:n]
+        u[n-1, n-1] = m[n-1, n-1]
 
-    z = forw_subst(l, b)
-    x = back_subst((np.c_[u, z]).astype(float))
+        z = forw_subst(l, b)
+        x = back_subst((np.c_[u, z]).astype(float))
     return x
 
 # LU factorization with EGPP
 def lu_pp(a, b):
-    n = len(a)
-    l = np.eye(n)
-    u = np.zeros((n, n))
-    p = np.eye(n)
-    m = np.copy(a).astype(float)
+    singular = is_singular(a)
+    if singular:
+        sys.exit('The typed matrix is not invertible')
+    else:
+        n = len(a)
+        l = np.eye(n)
+        u = np.zeros((n, n))
+        p = np.eye(n)
+        m = np.copy(a).astype(float)
 
-    for i in range(n-1):
-        # Rows
-        aux0, aux = abs(m[i+1:n, i]).max(), (abs(m[i+1:n, i]).argmax() + 1)
-        if(aux0 > abs(m[i, i])):
-            aux1 = m[i+aux, i:n]
-            aux2 = p[i+aux, :]
-            m[aux+i, i:n] = m[i, i:n]
-            p[aux+i, :] = p[i, :]
-            m[i, i:n] = aux1
-            p[i, :] = aux2
-            if i > 0:
-                aux3 = l[i+aux, 0:i-1]
-                l[i+aux, 0:i] = l[i, 0:i]
-                l[i, 0:i] = aux3
-        for j in range(i+1, n):
-            if m[i, i] != 0:
-                l[j, i] = m[j, i]/m[i, i]
-                m[j, i:n+1] = m[j, i:n+1]-(m[j, i]/m[i, i])*m[i, i:n+1]
-            else:
-                sys.exit('Divide by zero')
+        for i in range(n-1):
+            # Rows
+            aux0, aux = abs(m[i+1:n, i]).max(), (abs(m[i+1:n, i]).argmax() + 1)
+            if(aux0 > abs(m[i, i])):
+                aux1 = m[i+aux, i:n]
+                aux2 = p[i+aux, :]
+                m[aux+i, i:n] = m[i, i:n]
+                p[aux+i, :] = p[i, :]
+                m[i, i:n] = aux1
+                p[i, :] = aux2
+                if i > 0:
+                    aux3 = l[i+aux, 0:i-1]
+                    l[i+aux, 0:i] = l[i, 0:i]
+                    l[i, 0:i] = aux3
+            for j in range(i+1, n):
+                if m[i, i] != 0:
+                    l[j, i] = m[j, i]/m[i, i]
+                    m[j, i:n+1] = m[j, i:n+1]-(m[j, i]/m[i, i])*m[i, i:n+1]
+                else:
+                    sys.exit('Divide by zero')
 
-        u[i, i:n] = m[i, i:n]
-        u[i, i:n] = m[i, i:n]
-    u[n-1, n-1] = m[n-1, n-1]
+            u[i, i:n] = m[i, i:n]
+            u[i, i:n] = m[i, i:n]
+        u[n-1, n-1] = m[n-1, n-1]
 
-    z = forw_subst(l, np.dot(p, b))
-    x = back_subst((np.c_[u, z]).astype(float))
+        z = forw_subst(l, np.dot(p, b))
+        x = back_subst((np.c_[u, z]).astype(float))
     return x
 
 
@@ -137,6 +157,7 @@ def jacobi(a, b, x0, tol, nmax):
     u = -(np.triu(a, 1))
     t = np.dot(np.linalg.inv(d), (l+u))
     c = np.dot(np.linalg.inv(d), b)
+
     xant = x0
     e = 1000
     cont = 0
@@ -176,13 +197,16 @@ def sor(a, b, x0, w, tol, nmax):
     return result, res_cont, res_e
 
 # Auxiliar function for looping in iterative methods
+
+
 def iterate(e, tol, cont, nmax, t, xant, c):
-        while e > tol and cont < nmax:
-            xact = np.dot(t, xant)+c
-            e = np.linalg.norm(xant-xact)
-            xant = xact
-            cont+=1
-        return xact, cont, e
+    while e > tol and cont < nmax:
+        xact = np.dot(t, xant)+c
+        e = np.linalg.norm(xant-xact)
+        xant = xact
+        cont += 1
+    return xact, cont, e
+
 
 def forw_subst(l, b):
     n = len(l)
@@ -208,6 +232,13 @@ def back_subst(m):
     return x
 
 
+def is_singular(a):
+    if(np.linalg.det(a)):
+        return False
+    else:
+        return True
+
+
 if __name__ == "__main__":
 
     '''a = [[14, 6, -2, 3],
@@ -215,19 +246,23 @@ if __name__ == "__main__":
          [-7, 4, -23, 2],
          [1, -3, -2, 16]]
     b = [[12], [32], [-24], [14]]
-    
+        
+    print(gauss(a, b))
+    print(gauss_par(a, b))
+    print(gauss_tot(a, b))
+    print(lu_pp(a, b))
+    print(lu(a, b))'''
     a= [[3, -1, 1], [1, -8, -2], [1, 1, 5]]
-    b=[[-2], [1], [4]]'''
-    '''print(jacobi(a, b, 0, 0.0001, 100))
-    print(gseidel(a, b, 0, 0.0001, 100))
+    b=[[-2], [1], [4]]
+
+    print(jacobi(a, b, 0, 0.0001, 100))
+    '''print(gseidel(a, b, 0, 0.0001, 100))
     print(sor(a, b, 0, 0.5, 0.0001, 100))'''
     # Para testing con pivoteo total
-    a =[[1, 1, 1, 0, 0, 0],
+    '''a =[[1, 1, 1, 0, 0, 0],
         [4, 2, 1, 0, 0, 0],
         [0, 0, 0, 4, 2, 1],
         [0, 0, 0, 16, 4, 1],
         [4, 1, 0, -4, -1, 0],
         [2, 0, 0, 0, 0, 0]]
-    b= [[141],   [112.7],  [112.7],  [125.63],   [0],     [0]]
-    print(gauss_tot(a, b))
-    
+    b= [[141],   [112.7],  [112.7],  [125.63],   [0],     [0]]'''
